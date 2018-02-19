899b1397d0837642c1b7da5a2a585de9e225d726
rockysingh3 <rockygsingh@icloud.com>
2018-02-19 03:36:05 -0500

    Snake

--- Snake.java
+++ Snake.java
@@ -1,4 +1,17 @@
-Write a method findSpiral to traverse a 2D matrix of ints in a clockwise spiral order and append the elements to an output ArrayList if Integers.
+Write a method findSpiral to traverse a 2D matrix of ints in a clockwise spiral order and append the elements to an output
+ArrayList if Integers.
+
+Example:
+
+Input Matrix :
+
+    {1, 2, 3}
+
+    {4, 5, 6}
+
+    {7, 8, 9}
+
+Output ArrayList:[1, 2, 3, 6, 9, 8, 7, 4, 5]
 
 public static ArrayList<Integer> findSpiral(int[][] arr) {
 
